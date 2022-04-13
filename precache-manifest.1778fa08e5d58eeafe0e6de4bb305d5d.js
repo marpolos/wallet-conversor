27e5300191df1e54eb08fed7740ebfc8ab7f5528
Marcelle Monteiro <monteiro.bio@outlook.com>
2022-04-13 12:18:28 -0300

    Updates

---
+++ precache-manifest.1778fa08e5d58eeafe0e6de4bb305d5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe19d3b2c652c32a51ecc463e4f5e9dd",
+    "url": "/wallet-conversor/index.html"
+  },
+  {
+    "revision": "ba22c61781ffc86a1817",
+    "url": "/wallet-conversor/static/css/main.43187c1c.chunk.css"
+  },
+  {
+    "revision": "4d3374533223dfb8baba",
+    "url": "/wallet-conversor/static/js/2.23bd71ff.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/wallet-conversor/static/js/2.23bd71ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba22c61781ffc86a1817",
+    "url": "/wallet-conversor/static/js/main.22f391b3.chunk.js"
+  },
+  {
+    "revision": "d7f90e4babdd270dd92e",
+    "url": "/wallet-conversor/static/js/runtime-main.be769a86.js"
+  }
+]);
